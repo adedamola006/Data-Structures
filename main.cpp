4400bf4cd719a81c151566b76c77e069b3201a6b
Eniiyi Bishop Ajala <152617673+adedamola006@users.noreply.github.com>
2023-12-11 17:14:46 +0100

    Add files via upload
    This is a function to perform a PUSH operation on dynamically allocated stack containing real numbers.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-
+#include <stack>
 using namespace std;
 
 int main()
 {
    // Question 2: a program that scans whether a string supplied is a palindrome or not.
 
-   stack <int> my_stack;
-   my_stack.push(0);
-   my_stack.push(1);
-   my_stack.push(2);
-   my_stack.push(3);
-   my_stack.push(4);
+   stack <int> push_stack;
+   push_stack.push(0);
+   push_stack.push(1);
+   push_stack.push(2);
+   push_stack.push(3);
+   push_stack.push(4);
 
-   while (!my_stack.empty()) {
-    cout << " " << my_stack.top();
-    mystack.pop();
+   while (!push_stack.empty()) {
+    cout << " " << push_stack.top();
+    push_stack.pop();
    }
 }
